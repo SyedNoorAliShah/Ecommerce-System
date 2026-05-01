f1761ac8236257c9311e75ad70e665b9b07e41aa
SyedNoorAliShah <noorashah61@gmail.com>
2026-05-01 23:58:02 +0500

    Add K8s, Terraform, and GitHub Actions deployment

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+﻿variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+  default     = "156f9b3f-4714-4100-ad83-c22de61ace27"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  type        = string
+  default     = "ecommerce-rg"
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "eastus"
+}
